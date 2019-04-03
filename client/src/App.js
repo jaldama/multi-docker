7a8d1910d23950a0a2a1a0045734b2487745601a
jaldama <isayrhymes@hotmail.com>
2019-04-03 01:01:41 -0700

    push this

--- client/src/App.js
+++ client/src/App.js
@@ -12,7 +12,7 @@ class App extends Component {
         <div className="App">
           <header className="App-header">
             <img src={logo} className="App-logo" alt="logo" />
-            <h1 className="App-title">Fib Calculator</h1>
+            <h1 className="App-title">Fib Calculatorrrrrrrrr</h1>
           </header>
           <div>
             <Route exact path="/" component={Fib} />
